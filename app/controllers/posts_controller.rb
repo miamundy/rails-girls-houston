497875cbe7e33fd249cb148b9dcf7b51d60d4afd
mia mundy <miamundy@gmail.com>
2014-02-22 18:09:51 -0600

    added an edit option

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,4 +16,19 @@ class PostsController < ApplicationController
       render :new
     end
   end
+
+  def update
+    @post = Post.find_by_id(params[:id])
+    @post.update_attributes(params[:post])
+     redirect_to action: :show
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    render :show
+  end
+def edit
+    @post = Post.find(params[:id])
+    render :edit
+end
 end
